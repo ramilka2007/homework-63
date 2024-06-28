13e4c8e44583750d9599611709dfcc693281f781
ramilka <lilya1976.03.03@gmail.com>
2024-06-28 21:23:19 +0600

    Сделал header (заголовок 'My blog' и ссылки home, add, about и contacts); установил библиотеку бустрап.

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,34 @@
-import './App.css'
+import './App.css';
+import Toolbar from "./components/Toolbar/Toolbar";
+import {Link, Route, Routes} from "react-router-dom";
 
 const App = () => {
   return (
-    <></>
-  )
+      <>
+          <header>
+              <Toolbar/>
+          </header>
+          <main>
+              <Routes>
+                  <Route path="/"/>
+                  <Route path="/new-post"/>
+                  <Route path="/about"/>
+                  <Route path="/contacts"/>
+                  <Route
+                      path="*"
+                      element={
+                          <div>
+                              <h1 className={'mt-5 text-danger'}>Not found!</h1>
+                              <Link to="/" className={'btn btn-danger'}>
+                                  Go back!
+                              </Link>
+                          </div>
+                      }
+                  />
+              </Routes>
+          </main>
+      </>
+  );
 };
 
-export default App
+export default App;
